abc1a7c71c2c8f302129a255cb5587eca39e44f1
Gaurav Hoskote <gauravgourang.hoskote@phoenix.edu>
2025-08-05 17:06:49 -0500

    init

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-resource "aws_iam_role" "stepfn_role" {
-  name = "StepFunctionExecutionRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "states.amazonaws.com"
-      }
-    }]
-  })
-}
+# resource "aws_iam_role" "stepfn_role" {
+#   name = "StepFunctionExecutionRole"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Action = "sts:AssumeRole",
+#       Effect = "Allow",
+#       Principal = {
+#         Service = "states.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
 
 # resource "aws_cloudwatch_log_group" "stepfn_logs" {
 #   name              = "/aws/stepfunctions/example"
 #   retention_in_days = 7
 # }
 
-module "hello_world_stepfunction" {
-  source              = "./modules/stpfnc"
-  name                = "hello-world-stepfunction"
-  role_arn            = aws_iam_role.stepfn_role.arn
-  # log_group_arn       = aws_cloudwatch_log_group.stepfn_logs.arn
-  definition_file_path = "statemachines/sample_hello_world.json"
-}
+# module "hello_world_stepfunction" {
+#   source              = "./modules/stpfnc"
+#   name                = "hello-world-stepfunction"
+#   role_arn            = aws_iam_role.stepfn_role.arn
+#   # log_group_arn       = aws_cloudwatch_log_group.stepfn_logs.arn
+#   definition_file_path = "statemachines/sample_hello_world.json"
+# }
 
